95955bca71cf5ade5b688e061d5ef60a28e97fa4
ICTER Guru <icterguru@gmail.com>
2017-12-13 00:23:22 -0500

    HelloWorld.java program updated with third line

--- src/chapter00/HelloWorld.java
+++ src/chapter00/HelloWorld.java
@@ -4,8 +4,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World!");
+		System.out.println("Hello World");
 		System.out.print("Welcome to Java Programming");
+		
+		System.out.print("\nThis is my First Java Program");
 	}
 
 }
